1030158ae992cd6fd4d8151cdd0fc0b216fa78d1
Martin <mlienhard@indeed.com>
2021-03-11 07:47:57 -0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Martin Lienhard on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
